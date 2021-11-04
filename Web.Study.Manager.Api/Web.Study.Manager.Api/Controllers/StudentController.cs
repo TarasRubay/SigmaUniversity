5cbf36663cc24e5bc852d2077c2327ec043139ff
TarasRubay <84627325+TarasRubay@users.noreply.github.com>
2021-11-04 10:31:02 +0200

    Completed homeTaskController and dependecy injection, also add POST request.

---
+++ Web.Study.Manager.Api/Web.Study.Manager.Api/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using ADO.NET;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Study.Manager.Api.Controllers
+{
+    public class StudentController : Controller
+    {
+        //private static string _connectionString = "Data Source=DESKTOP-QTE7RK8\\SQLEXPRESS;Initial Catalog=University;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private static string _connectionString;
+            private static void SetConnectionString()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile($"appsettings.json", true, true);
+            var configuration = builder.Build();
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        private static IRepository<Student> GetStudentRepository()
+        {
+            return new StudentRepository(_connectionString);
+        }
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            SetConnectionString();
+            IRepository<Student> repository = GetStudentRepository();
+            
+            return View(repository.GetAll());
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
